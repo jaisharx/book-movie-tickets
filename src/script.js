3f04d636258cf721397e01720d4f4405ff6b70b8
Jai Sharma <sendtojsharma@gmail.com>
2020-09-02 14:52:08 +0530

    updated

--- src/script.js
+++ src/script.js
@@ -1,64 +1,126 @@
-const isSeatOccupied = (chances) => {
-    if (chances > 0.5) return true;
-    else return false;
-};
-
-const generateSeats = (numOfRows, eachRowSize) => {
-    const container = document.querySelector('.container');
-
+// generating bunch of seats programmatically
+(function generateSeatsAndPopulate(numOfRows = 10, eachRowSize = 18) {
     for (let i = 0; i < numOfRows; i++) {
-        let row = document.createElement('div');
+        // creating a row
+        const row = document.createElement('div');
         row.classList.add('row');
 
         for (let j = 0; j < eachRowSize; j++) {
-            const randInt = Math.random();
-            let seat = document.createElement('div');
-
+            // creating a seat
+            const seat = document.createElement('div');
             seat.classList.add('seat'); // adding default seat class
 
-            if (isSeatOccupied(randInt)) {
-                seat.classList.add('occupied'); // randomly occupying seats
-            }
-
-            row.appendChild(seat);
+            row.appendChild(seat); // adding all seats to the row
         }
 
-        container.appendChild(row);
+        document.querySelector('.container').appendChild(row); // adding each rows to the container
     }
-};
-
-generateSeats(10, 18);
+})();
 
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row .seat:not(.occupied)');
-
-const count = document.getElementById('count');
-const total = document.getElementById('total');
-
+const seats = document.querySelectorAll('.row .seat');
+const text = document.querySelector('.text');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
+
+const saveToLocalStorage = (key, value) => {
+    localStorage.setItem(key, value);
+};
 
 const updateSelectedSeats = () => {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     const selectedSeatsCount = selectedSeats.length;
+    const price = selectedSeatsCount * ticketPrice;
+    let updateString = '';
 
-    count.innerText = selectedSeatsCount;
-    total.innerText = `$ ${ticketPrice * selectedSeatsCount}`;
+    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+    saveToLocalStorage('selectedSeatsData', JSON.stringify(seatsIndex));
+
+    if (selectedSeatsCount === 0) {
+        updateString = "You haven't picked any seats.";
+    } else if (selectedSeatsCount === 1) {
+        updateString = `
+        You have selected <span>${selectedSeatsCount}</span> seat for the price of <span>$${price}</span>.
+        `;
+    } else {
+        updateString = `
+        You have selected <span>${selectedSeatsCount}</span> seats for the price of <span>$${price}</span>.
+        `;
+    }
+
+    text.innerHTML = updateString;
 };
 
-// movie select event
+const randomlyOccupySeats = () => {
+    const occupiedSeats = [];
+
+    seats.forEach((seat) => {
+        const shouldWeOccupySeats = Math.round(Math.random());
+
+        if (shouldWeOccupySeats === 1) {
+            // yes occupy
+            seat.classList.add('occupied');
+        }
+
+        occupiedSeats.push(shouldWeOccupySeats);
+    });
+    saveToLocalStorage('occupiedSeatsData', JSON.stringify(occupiedSeats));
+};
+
+const useLSToOccupySeats = (occupiedSeatsArr) => {
+    for (let i = 0; i < seats.length; i++) {
+        if (occupiedSeatsArr[i] === 1) {
+            seats[i].classList.add('occupied');
+        }
+    }
+};
+
+const checkIfOccupiedSeatsDataExistsInLS = () => {
+    const occupiedSeats = JSON.parse(localStorage.getItem('occupiedSeatsData'));
+
+    if (occupiedSeats) {
+        useLSToOccupySeats(occupiedSeats);
+    } else {
+        randomlyOccupySeats(); // user's first visit or no localStorage obj found
+    }
+};
+
+const checkIfSelectedSeatsDataExistsInLS = () => {
+    const selectedSeatsArr = JSON.parse(localStorage.getItem('selectedSeatsData'));
+    console.log(selectedSeatsArr);
+
+    if(selectedSeatsArr){
+        selectedSeatsArr.forEach(index => {
+            seats[index].classList.add('selected');
+        });
+        updateSelectedSeats(); // for updating the text
+    }
+}
+
+
+(function populateUIWithLSData () {
+    checkIfOccupiedSeatsDataExistsInLS();
+    checkIfSelectedSeatsDataExistsInLS();
+    
+})();
+
+
+// movie pickup event
 movieSelect.addEventListener('change', (e) => {
+    const index = e.target.selectedIndex
     ticketPrice = +e.target.value;
+
+    saveToLocalStorage('movieData', JSON.stringify([index, ticketPrice]));
     updateSelectedSeats(); // for refreshing the amount
 });
 
 // seat click event
-container.addEventListener('click', e => {
+container.addEventListener('click', (e) => {
     const classes = e.target.classList;
 
-    if(classes.contains('seat') && !classes.contains('occupied')){
+    if (classes.contains('seat') && !classes.contains('occupied')) {
         e.target.classList.toggle('selected');
         updateSelectedSeats();
     }
-
 });
